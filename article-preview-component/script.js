f8f782a960b47552cf66e0aecaf8dce865b1a8dd
Viv3kkk <sudomailvivek@gmail.com>
2025-04-03 13:32:28 +0000

    add script.js

---
+++ article-preview-component/script.js
@@ -0,0 +1,7 @@
+const shareOption = document.querySelector('.share-option');
+const shareButton = document.querySelector('.share-button');
+
+shareButton.addEventListener('click', function () {
+  this.classList.toggle('active');
+  shareOption.classList.toggle('active');
+});
